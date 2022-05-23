befb7ad92ba437768913de3edcd434673aae885c
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-05-23 11:59:02 +0100

    test featured context

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,6 +5,9 @@ export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     // const {greeting, name} = this.context;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    
     return (
       <div>
         hello from FeaturedRooms
