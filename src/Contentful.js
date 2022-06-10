473db998cf882e43cd3ae5b60aad0e1753fec27c
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-06-10 17:43:07 +0100

    deleted yarn.lock

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,5 +2,5 @@ import { createClient } from 'contentful';
 
 export default createClient({
   space: process.env.REACT_APP_CONTENTFUL_API_SPACE,
-  accessToken: process.env.REACT_APP_CONTENTFUL_API_ACCESSTOKEN
+  accessToken: process.env.REACT_APP_CONTENTFUL_API_ACCESS_TOKEN
 })
